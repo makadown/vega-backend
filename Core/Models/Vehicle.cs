028fbd4b6453d185251725f5660326b62b020c6d
Mario Serrano <maseflores79@gmail.com>
2018-09-03 16:27:43 -0700

    Agregando el modelo de fotos y su migration

--- Core/Models/Vehicle.cs
+++ Core/Models/Vehicle.cs
@@ -32,9 +32,12 @@ namespace vega_backend.Core.Models
 
         public ICollection<VehicleFeature> Features { get; set; }
 
+        public ICollection<Photo> Photos { get; set; }
+
         public Vehicle()
         {
             Features = new Collection<VehicleFeature>();
+            Photos = new Collection<Photo>();
         }
     }
 }
